cc2712842e15948bf65b431a83fe3b1c908ed6f3
Edgarkimbugwe <kimbuny@gmail.com>
2024-07-19 22:44:51 +0000

    Updated `urls.py` to use custom error handlers for 404 and 500 errors.

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
+# from .views import test_500
 
 urlpatterns = [
+    # path('test-500/', views.test_500),
     path('', views.home, name='home')
 ]
