6e42d702657b3d4c3d2c4c934ba68b2e2baa2b9a
Edgarkimbugwe <kimbuny@gmail.com>
2024-07-19 22:44:30 +0000

    Implemented custom view functions for handling 404 and 500 errors in `views.py`.

--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Renders the home page
 def home(request):
     return render(request, 'index.html')
+
+
+# Custom 404 error page view
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
+
+# Custom 500 error page view
+def custom_500(request):
+    return render(request, '500.html', status=500)
+
+# def test_500(request):
+    # raise Exception("Test 500 error")
