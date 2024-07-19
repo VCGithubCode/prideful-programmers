47c5d464b4ba9a36773e1e91943598b7a41ec2ff
akirby23 <aoifekirby33@gmail.com>
2024-07-19 17:49:49 +0100

    Change debug settings for troubleshooting

--- pridefulproject/settings.py
+++ pridefulproject/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'FALSE' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'inclusive-spots-eeb5d9ac8ebf.herokuapp.com',
 '8000-vcgithubcod-pridefulpro-8r1r6tqbp3s.ws-eu115.gitpod.io'
